b3a07df82c0d3c8e00dd3fbcab19f8b083b31e30
Everson Vidal <vidal.dev07@gmail.com>
2026-03-28 01:34:26 -0400

    feat: Estructura base de la aplicación

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,14 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withViewTransitions } from '@angular/router';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync()]
+    providers: [
+        provideRouter(routes, withViewTransitions()),
+        provideAnimationsAsync(),
+        provideHttpClient(),
+    ],
 };
